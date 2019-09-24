2fce22f5382f1ef59fabdeb84182600ada6c59a9
Jiwan Bhandari <nbjiwan@gmail.com>
2019-09-24 04:11:38 -0700

    fix(doc): more jsdoc comments

--- release.config.js
+++ release.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   branch: 'master',
   fail: false,
+  plugins: [
+    [
+      '@semantic-release/git',
+      {
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
 };
